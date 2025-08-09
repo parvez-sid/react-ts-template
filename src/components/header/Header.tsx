97d401733692b91ba2630f997e51ee4d6b89edc6
Parvez Siddiqi <psiddique74@gmail.com>
2025-08-10 00:32:26 +0530

    lang dropdown

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,21 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next'
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 export default function Header() {
+  const [language, setLanguage] = useState<string>('en');
 
   const { i18n } = useTranslation()
 
   const changeLanguage = (lang: string) => i18n.changeLanguage(lang);
+
+  const handleLanguageChange = (value : string) => {
+    setLanguage(value);
+    changeLanguage(value)
+  }
 
   return (
     <header className="bg-gray-800 text-white p-4">
@@ -21,12 +30,26 @@ export default function Header() {
         </div>
 
         <div>
-            <Button variant='contained' size="small" onClick={() => changeLanguage('en')} className="px-4 py-2 bg-blue-500 text-white">
-                English
-            </Button>{" "}
-            <Button variant='contained' size="small" onClick={() => changeLanguage('hi')} className="px-4 py-2 bg-green-500 text-white">
-                हिंदी
-            </Button>
+            <Select
+                value={language}
+                sx={{
+                    bgcolor: language === 'en' ? 'primary.main' : 'success.main',
+                    color: 'white',
+                    '& .MuiSelect-select': {
+                        py: 1,
+                    },
+                    '& .MuiOutlinedInput-notchedOutline': {
+                        border: 'none',
+                    },
+                    '&:hover': {
+                        bgcolor: language === 'en' ? 'primary.dark' : 'success.dark',
+                    }
+                }}
+                onChange={(e) => handleLanguageChange(e.target.value)}
+            >
+                <MenuItem sx={{ color: 'text.primary' }} selected value={'en'}>English</MenuItem>
+                <MenuItem sx={{ color: 'text.primary' }} value={'hi'}>हिंदी</MenuItem>
+            </Select>
         </div>
       </nav>
     </header>
